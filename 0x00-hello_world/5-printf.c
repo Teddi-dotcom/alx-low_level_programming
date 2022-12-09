c39ea16a6a4c20377035be8ccc99550414b7b7bb
Teddi-dotcom <chikereesther798@gmail.com>
2022-12-08 17:45:50 -0800

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>                                                                                                              
+                                                                                                                                
+                                                                                                                                
+/**                                                                                                                             
+ *  * main - Entry point                                                                                                           
+ *   *                                                                                                                              
+ *    *                                                                                                                              
+ *     * Return: Always 0(success)                                                                                                    
+ *      */                                                                                                                             
+int main(void)                                                                                                                  
+{                                                                                                                               
+	        printf("\"Programming is like building a multilingual puzzle");                                                           
+		        return (0);                                                                                                             
+			                                                                                                                                
+}
